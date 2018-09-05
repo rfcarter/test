c287e4f7db87f78c13b1b36c0b76ffa41e807fc7
Robert Carter <Bob@Roberts-MacBook-Air.local>
2018-09-04 20:20:24 -0600

    Version 0

---
+++ pos_neg_zero_number.py
@@ -0,0 +1,7 @@
+my_num = int(input('Enter a number: '))
+if my_num == 0:
+    print ("The number you entered is zero:", my_num)
+elif my_num > 0:
+    print ("The number you entered is greater zero:", my_num)    
+else:
+    print ("The number you entered is less zero:", my_num)
