30f9427508e19c3e5cd0aabfd4061504754bfc66
Robert Carter <Bob@Roberts-MacBook-Air.local>
2018-09-04 19:30:32 -0600

    Version 0

---
+++ my_first_program.py
@@ -0,0 +1,33 @@
+my_first_var = 'Wohoo!'
+print(my_first_var)
+
+my_second_var = "I'm typing code."
+print(my_second_var)
+
+my_third_var = "Even though this doesn't feel like code, I've been told it is code."
+print(my_third_var) 
+
+my_combined_var = my_first_var + ' ' + my_second_var + ' ' + my_third_var 
+print(my_combined_var)
+
+my_first_num = 3
+print(my_first_num)
+
+my_second_num = 13
+print(my_second_num)
+
+quotient = my_second_num // my_first_num
+remainder = my_second_num % my_first_num
+
+print(quotient, remainder)
+
+my_str_var = 'Hello. My favorite number is '
+print(my_str_var)
+
+my_fav_num = 3
+print(my_fav_num)
+
+full_str = my_str_var + str(my_fav_num) 
+print(full_str)
+
+
