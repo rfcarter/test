179b6b450c52d5e3bac793cc0801176993616344
Robert Carter <rfcarter@att.net>
2018-10-22 14:33:00 -0600

    Version 0

---
+++ test4.py
@@ -0,0 +1,13 @@
+def matrix(size,fill_value):
+    matrix=[]
+    for _ in range(size):
+        row=[]
+        for _ in range(size):
+            row.append(fill_value)
+        matrix.append(row)
+    return matrix
+
+if __name__ == '__main__':
+    print(matrix(3,'abc'))
+    print("Here")
+
